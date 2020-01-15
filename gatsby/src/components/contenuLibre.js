7310b49f6723c90cb22430ef91a4db676fab46fd
Florian TRIBOULIN <ftr@atolcd.com>
2020-01-15 11:37:54 +0100

    Render first free content in home page
    Props taxoLibre is hardcoded for the moment
    
    Change-Id: Ia7db165fa73f2a07f23b798dc752c7ce413a6d45
    Reviewed-on: https://gerrit.atolcd.com/c/ternum-bfc/generateur-sites/+/118877
    Reviewed-by: Guillaume Poittevin <gpo@atolcd.com>
    Reviewed-by: Valentin Fayard <vfa@atolcd.com>
    Tested-by: Valentin Fayard <vfa@atolcd.com>

---
+++ gatsby/src/components/contenuLibre.js
@@ -0,0 +1,46 @@
+import { useStaticQuery, graphql } from "gatsby"
+import React from "react"
+import { zoneLibre, zoneLibreTitle } from "../styles/contenuLibre.module.scss"
+import ContentTransformer from "../components/content-transformer"
+
+const ContenuLibre = ({zoneTaxoLibre}) => {
+  const data = useStaticQuery(graphql`
+    query ContenuLibreQuery {
+      allNodeContenuLibre(sort: { fields: created, order: DESC }) {
+        edges {
+          node {
+            body {
+              processed
+            }
+            title
+            relationships {
+              field_taxonomie_zone_libre {
+                name
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+  const contenusLibre = data.allNodeContenuLibre.edges
+  const result = contenusLibre
+    .filter(
+      ({ node }) =>
+        node.relationships?.field_taxonomie_zone_libre?.name ===
+        zoneTaxoLibre
+    )
+    .shift()
+  return (
+    <>
+      {result && (
+        <div className={zoneLibre}>
+          <h2 className={zoneLibreTitle}>{result.node.title}</h2>
+          <ContentTransformer content={result.node.body?.processed} />
+        </div>
+      )}
+    </>
+  )
+}
+
+export default ContenuLibre
--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import ListActualites from "../components/listActualites"
+import ContenuLibre from "../components/contenuLibre"
 import {
   background,
   backgroundText,
@@ -12,21 +13,24 @@ import {
 import "../styles/mixins.scss"
 import "../styles/animations.scss"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <div className={backgroundContainer}>
-      <div className={backgroundWrapper}>
-        <img className={background} alt="bg" src="lune-2000px.jpg"></img>
-        <div className={backgroundText}>
-          <p>Bienvenue sur ce nouveau thème !</p>
+const IndexPage = () => {
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <div className={backgroundContainer}>
+        <div className={backgroundWrapper}>
+          <img className={background} alt="bg" src="lune-2000px.jpg"></img>
+          <div className={backgroundText}>
+            <p>Bienvenue sur ce nouveau thème !</p>
+          </div>
         </div>
       </div>
-    </div>
-    <div className={sectionFirst}>
-      <ListActualites />
-    </div>
-  </Layout>
-)
+      <div className={sectionFirst}>
+        <ContenuLibre zoneTaxoLibre="zone_accueil_1"/>
+        <ListActualites />
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage
