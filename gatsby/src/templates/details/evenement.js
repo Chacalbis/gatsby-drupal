1a3aa36e21b3cd1c2c665340a79240707b963eee
Florian TRIBOULIN <ftr@atolcd.com>
2020-02-04 11:23:46 +0100

    Render Event details
    Change-Id: Ia781fb28ae5ce15cecfe83dfdf422fe715260748
    Reviewed-on: https://gerrit.atolcd.com/c/ternum-bfc/generateur-sites/+/120907
    Reviewed-by: Valentin Fayard <vfa@atolcd.com>
    Tested-by: Valentin Fayard <vfa@atolcd.com>

--- gatsby/src/templates/details/evenement.js
+++ gatsby/src/templates/details/evenement.js
@@ -4,69 +4,88 @@ import Layout from "../../components/layout"
 import SEO from "../../components/seo"
 import NonStretchedImage from "../../components/non-stretched-image"
 import AdditionalInformation from "../../components/additional-information"
+import ContentTransformer from "../../components/content-transformer"
+import {
+  pageEvent,
+  pageEventAdditionalInfos,
+  evenement,
+  eventImg,
+  eventDate,
+  eventDateFrom,
+  eventDateTo,
+  eventTaxo,
+  eventTitle,
+  eventContent,
+  eventFooter,
+  eventFooterBack,
+} from "../../styles/detailsEvent.module.scss"
+
+const EvenementContent = ({ event }) => (
+  <article className={evenement}>
+    <div className={eventImg}>
+      {event.relationships?.field_image_event?.localFile?.childImageSharp
+        ?.fluid && (
+        <NonStretchedImage
+          alt={event.field_image_event.alt}
+          {...event.relationships.field_image_event.localFile.childImageSharp}
+        />
+      )}
+    </div>
+    <div className={eventDate}>
+      <span className={eventDateFrom}>Du {event.field_date_de_debut}</span>
+      <span className={eventDateTo}> au {event.field_date_de_fin}</span>
+    </div>
+    <div className={eventTaxo}>
+      {event.relationships.field_taxonomie_thematique && (
+        <>
+          {event.relationships.field_taxonomie_thematique.map(taxo => (
+            <Link key={taxo.path.alias} to={taxo.path.alias}>
+              {taxo.name}{" "}
+            </Link>
+          ))}
+        </>
+      )}
+    </div>
+    <h2 className={eventTitle}>{event.title}</h2>
+    <div className={eventContent}>
+      {event.body.processed && (
+        <ContentTransformer content={event.body.processed} />
+      )}
+    </div>
+  </article>
+)
+
+const EvenementFooter = () => (
+  <div className={eventFooter}>
+    <div className={eventFooterBack}>
+      <Link to="../event">Retour à la liste des évènements</Link>
+    </div>
+  </div>
+)
+
+const EvenementInfos = ({ event }) => (
+  <>
+    {event.field_infos_complementaires && (
+      <section className={pageEventAdditionalInfos}>
+        <AdditionalInformation node={event} />
+      </section>
+    )}
+  </>
+)
 
 const EvenementTemplate = ({ data }) => {
   const evenement = data.nodeEvenements
   return (
     <Layout>
       <SEO title={evenement.title} description={evenement.body.summary} />
-      <div key={evenement.path.alias}>
-        <EvenementHeader event={evenement} />
+      <section className={pageEvent}>
         <EvenementContent event={evenement} />
-      </div>
+        <EvenementFooter />
+      </section>
+      <EvenementInfos event={evenement} />
     </Layout>
   )
 }
-
-const EvenementHeader = ({ event }) => (
-  <div>
-    <h2>
-      <Link
-        to={event.path.alias}
-        dangerouslySetInnerHTML={{ __html: event.title }}
-      />
-    </h2>
-    <EvenementDetails event={event} />
-  </div>
-)
-
-const EvenementDetails = ({ event }) => (
-  <div>
-    {event.relationships?.field_taxonomie_evenement && (
-      <Link
-        to={event.relationships.field_taxonomie_evenement.path.alias}
-        dangerouslySetInnerHTML={{
-          __html: event.relationships?.field_taxonomie_evenement.name,
-        }}
-      />
-    )}
-    <div>
-      {event.field_date_de_debut && event.field_date_de_debut}
-      {` - `}
-      {event.field_date_de_fin && event.field_date_de_fin}
-    </div>
-    <div>
-      {event.field_url_lieu && (
-        <a href={event.field_url_lieu.uri}>
-          <span>{event.field_url_lieu.title}</span>
-        </a>
-      )}
-    </div>
-  </div>
-)
-
-const EvenementContent = ({ event }) => (
-  <div>
-    <div dangerouslySetInnerHTML={{ __html: event.body?.processed }} />
-    {event.relationships?.field_image_event?.localFile?.childImageSharp
-      ?.fluid && (
-      <NonStretchedImage
-        {...event.relationships.field_image_event.localFile.childImageSharp}
-      />
-    )}
-    <AdditionalInformation node={event} />
-  </div>
-)
 
 export default EvenementTemplate
 
@@ -80,8 +99,8 @@ export const query = graphql`
       path {
         alias
       }
-      field_date_de_debut(locale: "fr", formatString: "DD MMMM YYYY, h:mm a")
-      field_date_de_fin(locale: "fr", formatString: "DD MMMM YYYY, h:mm a")
+      field_date_de_debut(locale: "fr", formatString: "DD/MM/YY à hh:mm")
+      field_date_de_fin(locale: "fr", formatString: "DD/MM/YY à hh:mm")
       field_image_event {
         alt
       }
@@ -97,10 +116,7 @@ export const query = graphql`
           localFile {
             childImageSharp {
               fluid(quality: 100) {
-                src
-                presentationWidth
-                presentationHeight
-                aspectRatio
+                ...GatsbyImageSharpFluid
               }
             }
           }
