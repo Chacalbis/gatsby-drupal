49d76e2eee8818e8c37d136265e2646390284be0
Florian TRIBOULIN <ftr@atolcd.com>
2020-02-05 10:46:43 +0100

    Render Events listing without filters
    Missing filters and backgroundImage in Layout
    
    Change-Id: I57419e8ec6e477594c5061396c9e9fa4bf633b03
    Reviewed-on: https://gerrit.atolcd.com/c/ternum-bfc/generateur-sites/+/120001
    Reviewed-by: Valentin Fayard <vfa@atolcd.com>
    Tested-by: Valentin Fayard <vfa@atolcd.com>

--- gatsby/src/templates/list/evenements.js
+++ gatsby/src/templates/list/evenements.js
@@ -3,83 +3,108 @@ import { Link, graphql } from "gatsby"
 import Layout from "../../components/layout"
 import Pagination from "../../components/pagination/pagination"
 import NonStretchedImage from "../../components/non-stretched-image"
+import ContenuLibre from "../../components/contenuLibre"
+import SummaryTransformer from "../../components/summary-transformer"
+import {
+  listEventsContainer,
+  eventsContainer,
+  eventsItem,
+  eventsItemImg,
+  eventsItemDate,
+  eventsItemDateFrom,
+  eventsItemDateTo,
+  eventsItemTaxo,
+  eventsItemTitle,
+  eventsItemResume,
+  zoneEventsLibreHaut,
+  zoneEventsLibreBas,
+} from "../../styles/listEvents.module.scss"
+
+const EvenementInfos = ({ evenement }) => (
+  <>
+    <div className={eventsItemDate}>
+      <span className={eventsItemDateFrom}>
+        Du {evenement.field_date_de_debut}
+      </span>
+      <span className={eventsItemDateTo}>
+        {" "}
+        au {evenement.field_date_de_fin}
+      </span>
+    </div>
+    <div className={eventsItemTaxo}>
+      {evenement.relationships.field_taxonomie_thematique && (
+        <Link
+          to={evenement.relationships.field_taxonomie_thematique[0].path.alias}
+        >
+          {evenement.relationships.field_taxonomie_thematique[0].name}
+        </Link>
+      )}
+    </div>
+    <div className={eventsItemTitle}>
+      <Link to={evenement.path.alias}>{evenement.title}</Link>
+    </div>
+    {evenement.body && (
+      <div
+        className={eventsItemResume}
+        dangerouslySetInnerHTML={{
+          __html: SummaryTransformer(evenement.body, 200),
+        }}
+      />
+    )}
+  </>
+)
+
+const EvenementImg = ({ evenement }) => (
+  <div className={eventsItemImg}>
+    {evenement.relationships.field_image_event?.localFile?.childImageSharp
+      ?.fluid && (
+      <Link to={evenement.path.alias}>
+        <NonStretchedImage
+          {...evenement.relationships.field_image_event.localFile
+            .childImageSharp}
+        />
+      </Link>
+    )}
+  </div>
+)
+
+const RenderEvenements = ({ eventsData }) => {
+  const events = eventsData.allNodeEvenements.edges
+  return (
+    <>
+      {events.map(({ node }) => (
+        <div className={eventsItem}>
+          <EvenementImg evenement={node} />
+          <EvenementInfos evenement={node} />
+        </div>
+      ))}
+    </>
+  )
+}
 
 const EvenementsTemplate = ({ data, pageContext }) => {
   const { currentPage, numPages, baseLink } = pageContext
   return (
     <Layout>
-      <div>
-        <h5>
-          Page {currentPage} sur {numPages}
-        </h5>
-      </div>
-      <div>
-        {data.allNodeEvenements.edges.map(({ node }) => {
-          return (
-            <div>
-              <EvenementHeader event={node} />
-              <EvenementContent event={node} />
-            </div>
-          )
-        })}
-      </div>
-      <Pagination
-        currentPage={currentPage}
-        numPages={numPages}
-        contextPage={baseLink}
-      />
+      <section className={zoneEventsLibreHaut}>
+        <ContenuLibre zoneTaxoLibre="zone_evenements_haut" />
+      </section>
+      <section className={listEventsContainer}>
+        <div className={eventsContainer}>
+          <RenderEvenements eventsData={data} />
+        </div>
+        <Pagination
+          currentPage={currentPage}
+          numPages={numPages}
+          contextPage={baseLink}
+        />
+      </section>
+      <section className={zoneEventsLibreBas}>
+        <ContenuLibre zoneTaxoLibre="zone_evenements_bas" />
+      </section>
     </Layout>
   )
 }
-
-const EvenementHeader = ({ event }) => (
-  <div>
-    <h2>
-      <Link
-        to={event.path.alias}
-        dangerouslySetInnerHTML={{ __html: event.title }}
-      />
-    </h2>
-    <EvenementDetails event={event} />
-  </div>
-)
-
-const EvenementContent = ({ event }) => (
-  <div>
-    <div dangerouslySetInnerHTML={{ __html: event.body?.processed }} />
-    {event.relationships?.field_image_event?.localFile?.childImageSharp
-      ?.fluid && (
-      <NonStretchedImage
-        {...event.relationships.field_image_event.localFile.childImageSharp}
-      />
-    )}
-  </div>
-)
-
-const EvenementDetails = ({ event }) => (
-  <div>
-    {event.relationships?.field_taxonomie_evenement && (
-      <Link
-        to={event.relationships.field_taxonomie_evenement.path.alias}
-        dangerouslySetInnerHTML={{
-          __html: event.relationships?.field_taxonomie_evenement.name,
-        }}
-      />
-    )}
-    <div>
-      {event.field_date_de_debut && event.field_date_de_debut}
-      {` - `}
-      {event.field_date_de_fin && event.field_date_de_fin}
-    </div>
-    <div>
-      {event.field_url_lieu && (
-        <a href={event.field_url_lieu.uri}>
-          <span>{event.field_url_lieu.title}</span>
-        </a>
-      )}
-    </div>
-  </div>
-)
 
 export default EvenementsTemplate
 
@@ -90,16 +115,14 @@ export const query = graphql`
         node {
           title
           body {
+            summary
             processed
           }
           path {
             alias
           }
-          field_date_de_debut(
-            locale: "fr"
-            formatString: "DD MMMM YYYY, h:mm a"
-          )
-          field_date_de_fin(locale: "fr", formatString: "DD MMMM YYYY, h:mm a")
+          field_date_de_debut(locale: "fr", formatString: "DD/MM/YY")
+          field_date_de_fin(locale: "fr", formatString: "DD/MM/YY")
           field_image_event {
             alt
           }
