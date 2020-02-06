9f8741e760f3fc0647a69ccaffced731c8090fea
Florian TRIBOULIN <ftr@atolcd.com>
2020-02-06 14:18:55 +0100

    Fix error in Partenaires when img have SVG extension
    Change-Id: Iabe44ee7ae0a106c8f0a15e8af8248304499fafb
    Reviewed-on: https://gerrit.atolcd.com/c/ternum-bfc/generateur-sites/+/121213
    Reviewed-by: Valentin Fayard <vfa@atolcd.com>
    Tested-by: Valentin Fayard <vfa@atolcd.com>

--- gatsby/src/components/listPartenaires.js
+++ gatsby/src/components/listPartenaires.js
@@ -1,4 +1,4 @@
-import { Link, graphql, useStaticQuery } from "gatsby"
+import { graphql, useStaticQuery } from "gatsby"
 import React from "react"
 import NonStretchedImage from "./non-stretched-image"
 import {
@@ -8,14 +8,37 @@ import {
   partenaireItemImg,
 } from "../styles/listPartenaires.module.scss"
 
-const Partenaire = ({ partenaire }) => (
-  <Link to={partenaire.url}>
-    <NonStretchedImage
-      className={partenaireItemImg}
-      {...partenaire.relationships.image_direct_link.localFile.childImageSharp}
-    />
-  </Link>
-)
+const Partenaire = ({ partenaire }) => {
+  let image = ""
+  if (
+    partenaire.relationships.image_direct_link.localFile.extension === "svg"
+  ) {
+    image = (
+      <img
+        className={partenaireItemImg}
+        src={partenaire.relationships.image_direct_link.localFile.publicURL}
+        alt={partenaire.image_direct_link.alt || ""}
+      />
+    )
+  } else if (
+    partenaire.relationships?.image_direct_link?.localFile?.childImageSharp
+      .fluid
+  ) {
+    image = (
+      <NonStretchedImage
+        alt={partenaire.image_direct_link.alt}
+        className={partenaireItemImg}
+        {...partenaire.relationships.image_direct_link.localFile
+          .childImageSharp}
+      />
+    )
+  }
+  return (
+    <a target="_blank" rel="noopener noreferrer" href={partenaire.url}>
+      {image}
+    </a>
+  )
+}
 
 const RenderPartenaires = ({ partenairesData }) => {
   return partenairesData.allDirectLinkEntityDirectLinkEntity.edges.map(
@@ -37,9 +60,14 @@ const ListPartenaires = () => {
         edges {
           node {
             url
+            image_direct_link {
+              alt
+            }
             relationships {
               image_direct_link {
                 localFile {
+                  extension
+                  publicURL
                   childImageSharp {
                     fluid {
                       ...GatsbyImageSharpFluid
