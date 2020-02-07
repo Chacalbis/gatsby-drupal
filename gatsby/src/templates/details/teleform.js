e21da39404fd24b2737b343ff84f25a194936e94
Florian TRIBOULIN <ftr@atolcd.com>
2020-02-07 16:04:01 +0100

    Render Teleform details
    Change-Id: If8e6d0715c3a104709f9d023a1ff68b9f33548ba
    Reviewed-on: https://gerrit.atolcd.com/c/ternum-bfc/generateur-sites/+/121498
    Reviewed-by: Valentin Fayard <vfa@atolcd.com>
    Tested-by: Matthieu Girardeau <mgi@atolcd.com>

--- gatsby/src/templates/details/teleform.js
+++ gatsby/src/templates/details/teleform.js
@@ -2,6 +2,21 @@ import React from "react"
 import { graphql, Link } from "gatsby"
 import Layout from "../../components/layout"
 import SEO from "../../components/seo"
+import ContentTransformer from "../../components/content-transformer"
+import {
+  teleformContainer,
+  teleformulaire,
+  teleformTitle,
+  teleformTaxo,
+  teleformTaxoItem,
+  teleformFooter,
+  teleformFooterDate,
+  teleformLinks,
+  teleformLinksItem,
+  teleformLinksList,
+  teleformLinksTitle,
+  teleformLinksItemTitle,
+} from "../../styles/detailsTeleform.module.scss"
 
 const TeleformTemplate = ({ data }) => {
   const teleformulaire = data.nodeTeleformulaires
@@ -10,44 +25,62 @@ const TeleformTemplate = ({ data }) => {
     <Layout>
       <SEO
         title={teleformulaire.title}
-        description={teleformulaire.body.summary}
+        description={teleformulaire.body?.summary}
       />
-      <div key={teleformulaire.path.alias}>
+      <section className={teleformContainer}>
         <TeleformHeader teleform={teleformulaire} />
-        <TeleformContent teleform={teleformulaire} />
-      </div>
+        <TeleformLinks teleform={teleformulaire} />
+        <TeleformFooter teleform={teleformulaire} />
+      </section>
     </Layout>
   )
 }
 
 const TeleformHeader = ({ teleform }) => (
-  <h2>
-    <Link
-      to={teleform.path.alias}
-      dangerouslySetInnerHTML={{ __html: teleform.title }}
-    />
-  </h2>
-)
-
-const TeleformContent = ({ teleform }) => (
-  <div>
-    <div dangerouslySetInnerHTML={{ __html: teleform.body?.processed }} />
-    <TeleformLinks teleform={teleform} />
-  </div>
+  <article className={teleformulaire}>
+    <h1 className={teleformTitle}>{teleform.title}</h1>
+    {teleform.relationships.field_taxonomie_teleformulaire?.length > 0 && (
+      <div className={teleformTaxo}>
+        {teleform.relationships.field_taxonomie_teleformulaire.map(taxo => (
+          <span className={teleformTaxoItem}>
+            <Link to={taxo.path.alias}>{taxo.name}</Link>
+          </span>
+        ))}
+      </div>
+    )}
+    {teleform.body && <ContentTransformer content={teleform.body.processed} />}
+  </article>
 )
 
 const TeleformLinks = ({ teleform }) => {
   return (
-    <ul>
-      <h3>Liens démarches simplifiées</h3>
-      {teleform.field_lien_demarches_simplifiees.map(link => (
-        <li>
-          <a href={link.uri}>{link.title}</a>
-        </li>
-      ))}
-    </ul>
+    <div className={teleformLinks}>
+      <ul className={teleformLinksList}>
+        {teleform.field_lien_demarches_simplifiees?.length > 0 && (
+          <>
+            <h3 className={teleformLinksTitle}>Liens démarches simplifiées</h3>
+            {teleform.field_lien_demarches_simplifiees.map(link => (
+              <>
+                <li className={teleformLinksItem}>
+                  <a target="_blank" rel="noopener noreferrer" href={link.uri}>
+                    <p className={teleformLinksItemTitle}>{link.title}</p>
+                  </a>
+                </li>
+                <br />
+              </>
+            ))}
+          </>
+        )}
+      </ul>
+    </div>
   )
 }
+
+const TeleformFooter = ({ teleform }) => (
+  <div className={teleformFooter}>
+    <div className={teleformFooterDate}>posté le {teleform.created}</div>
+  </div>
+)
 
 export default TeleformTemplate
 
@@ -55,6 +88,7 @@ export const query = graphql`
   query($slug: String!) {
     nodeTeleformulaires(path: { alias: { eq: $slug } }) {
       title
+      created(formatString: "DD/MM/YYYY", locale: "fr")
       body {
         summary
         processed
@@ -65,6 +99,14 @@ export const query = graphql`
       field_lien_demarches_simplifiees {
         title
         uri
+      }
+      relationships {
+        field_taxonomie_teleformulaire {
+          name
+          path {
+            alias
+          }
+        }
       }
     }
   }
