2ec80dabaebe0a5c62a2ba7f22f89c5425280045
Florian TRIBOULIN <ftr@atolcd.com>
2020-02-12 16:29:35 +0100

    Issues #82084 | Modify teleform in order to allow displaay in ContenuLibre area
    Change-Id: I168af8e325266141decd4e0d31461eaf312a369f
    Reviewed-on: https://gerrit.atolcd.com/c/ternum-bfc/generateur-sites/+/121956
    Reviewed-by: Valentin Fayard <vfa@atolcd.com>
    Tested-by: Valentin Fayard <vfa@atolcd.com>

--- gatsby/src/components/contenuLibre.js
+++ gatsby/src/components/contenuLibre.js
@@ -1,7 +1,8 @@
 import { useStaticQuery, graphql } from "gatsby"
 import React from "react"
 import { zoneLibre, zoneLibreTitle } from "../styles/contenuLibre.module.scss"
-import ContentTransformer from "../components/content-transformer"
+import ContentTransformer from "./content-transformer"
+import Teleformulaire from "./teleformulaire/teleformulaire"
 
 const ContenuLibre = ({ zoneTaxoLibre }) => {
   const data = useStaticQuery(graphql`
@@ -9,8 +10,12 @@ const ContenuLibre = ({ zoneTaxoLibre }) => {
       allNodeContenuLibre(sort: { fields: created, order: DESC }) {
         edges {
           node {
+            created(formatString: "YYYY/MM/DD", locale: "en")
             body {
               processed
+            }
+            internal {
+              type
             }
             title
             relationships {
@@ -21,25 +26,78 @@ const ContenuLibre = ({ zoneTaxoLibre }) => {
           }
         }
       }
+      allNodeTeleformulaires(sort: { fields: created, order: DESC }) {
+        edges {
+          node {
+            title
+            created(formatString: "YYYY/MM/DD", locale: "en")
+            body {
+              summary
+              processed
+            }
+            path {
+              alias
+            }
+            field_lien_demarches_simplifiees {
+              title
+              uri
+            }
+            internal {
+              type
+            }
+            relationships {
+              field_taxonomie_zone_libre {
+                name
+              }
+              field_taxonomie_teleformulaire {
+                name
+                path {
+                  alias
+                }
+              }
+            }
+          }
+        }
+      }
     }
   `)
   const contenusLibre = data.allNodeContenuLibre.edges
-  const result = contenusLibre
+  const teleformulaires = data.allNodeTeleformulaires.edges
+  const resultTeleform = teleformulaires
     .filter(
       ({ node }) =>
         node.relationships?.field_taxonomie_zone_libre?.name === zoneTaxoLibre
     )
     .shift()
-  return (
-    <>
-      {result && (
-        <div className={zoneLibre}>
-          <h2 className={zoneLibreTitle}>{result.node.title}</h2>
-          <ContentTransformer content={result.node.body?.processed} />
-        </div>
-      )}
-    </>
-  )
+  const resultContenuLibre = contenusLibre
+    .filter(
+      ({ node }) =>
+        node.relationships?.field_taxonomie_zone_libre?.name === zoneTaxoLibre
+    )
+    .shift()
+  const bothNodes = [resultTeleform, resultContenuLibre]
+  const result = bothNodes
+    .sort((a, b) => {
+      if (a.node.created > b.node.created) return -1
+      if (a.node.created < b.node.created) return 1
+      return 0
+    })
+    .shift()
+  if (!result) return null
+  if (result.node.internal.type === "node__contenu_libre") {
+    return (
+      <div className={zoneLibre}>
+        <h2 className={zoneLibreTitle}>{result.node.title}</h2>
+        <ContentTransformer content={result.node.body?.processed} />
+      </div>
+    )
+  } else {
+    return (
+      <div className={zoneLibre}>
+        <Teleformulaire zoneLibre={true} teleformulaire={result.node} />
+      </div>
+    )
+  }
 }
 
 export default ContenuLibre
--- gatsby/src/templates/details/teleform.js
+++ gatsby/src/templates/details/teleform.js
@@ -1,22 +1,8 @@
 import React from "react"
-import { graphql, Link } from "gatsby"
+import { graphql } from "gatsby"
 import Layout from "../../components/layout"
 import SEO from "../../components/seo"
-import ContentTransformer from "../../components/content-transformer"
-import {
-  teleformContainer,
-  teleformulaire,
-  teleformTitle,
-  teleformTaxo,
-  teleformTaxoItem,
-  teleformFooter,
-  teleformFooterDate,
-  teleformLinks,
-  teleformLinksItem,
-  teleformLinksList,
-  teleformLinksTitle,
-  teleformLinksItemTitle,
-} from "../../styles/detailsTeleform.module.scss"
+import Teleformulaire from "../../components/teleformulaire/teleformulaire"
 
 const TeleformTemplate = ({ data }) => {
   const teleformulaire = data.nodeTeleformulaires
@@ -26,60 +12,10 @@ const TeleformTemplate = ({ data }) => {
         title={teleformulaire.title}
         description={teleformulaire.body?.summary}
       />
-      <section className={teleformContainer}>
-        <TeleformHeader teleform={teleformulaire} />
-        <TeleformLinks teleform={teleformulaire} />
-        <TeleformFooter teleform={teleformulaire} />
-      </section>
+      <Teleformulaire teleformulaire={teleformulaire} />
     </Layout>
   )
 }
-
-const TeleformHeader = ({ teleform }) => (
-  <article className={teleformulaire}>
-    <h1 className={teleformTitle}>{teleform.title}</h1>
-    {teleform.relationships.field_taxonomie_teleformulaire?.length > 0 && (
-      <div className={teleformTaxo}>
-        {teleform.relationships.field_taxonomie_teleformulaire.map(taxo => (
-          <span className={teleformTaxoItem}>
-            <Link to={taxo.path.alias}>{taxo.name}</Link>
-          </span>
-        ))}
-      </div>
-    )}
-    {teleform.body && <ContentTransformer content={teleform.body.processed} />}
-  </article>
-)
-
-const TeleformLinks = ({ teleform }) => {
-  return (
-    <div className={teleformLinks}>
-      <ul className={teleformLinksList}>
-        {teleform.field_lien_demarches_simplifiees?.length > 0 && (
-          <>
-            <h3 className={teleformLinksTitle}>Liens démarches simplifiées</h3>
-            {teleform.field_lien_demarches_simplifiees.map(link => (
-              <>
-                <li className={teleformLinksItem}>
-                  <a target="_blank" rel="noopener noreferrer" href={link.uri}>
-                    <p className={teleformLinksItemTitle}>{link.title}</p>
-                  </a>
-                </li>
-                <br />
-              </>
-            ))}
-          </>
-        )}
-      </ul>
-    </div>
-  )
-}
-
-const TeleformFooter = ({ teleform }) => (
-  <div className={teleformFooter}>
-    <div className={teleformFooterDate}>posté le {teleform.created}</div>
-  </div>
-)
 
 export default TeleformTemplate
 
