907d2f2a3772cc26f5d0c916039db1c7c983cb5b
Florian TRIBOULIN <ftr@atolcd.com>
2020-02-13 16:23:17 +0100

    Add Filter by taxo in listing Events
    Change-Id: Id0291a7799f203be8e08135df49dffd486267ccd
    Reviewed-on: https://gerrit.atolcd.com/c/ternum-bfc/generateur-sites/+/122072
    Reviewed-by: Guillaume Poittevin <gpo@atolcd.com>
    Reviewed-by: Valentin Fayard <vfa@atolcd.com>
    Tested-by: Valentin Fayard <vfa@atolcd.com>

--- gatsby/src/templates/list/evenements.js
+++ gatsby/src/templates/list/evenements.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link, graphql } from "gatsby"
 import Layout from "../../components/layout"
 import Pagination from "../../components/pagination/pagination"
@@ -18,41 +18,129 @@ import {
   eventsItemResume,
   zoneEventsLibreHaut,
   zoneEventsLibreBas,
+  eventsFilters,
+  eventsFilterByPeriod,
+  eventsFilterByTaxo,
+  eventsFiltersTaxo,
+  eventsFiltersTaxoActive,
+  eventsFilterDelete,
 } from "../../styles/listEvents.module.scss"
 
-const EvenementInfos = ({ evenement }) => (
-  <>
-    <div className={eventsItemDate}>
-      <span className={eventsItemDateFrom}>
-        Du {evenement.field_date_de_debut}
-      </span>
-      <span className={eventsItemDateTo}>
-        {" "}
-        au {evenement.field_date_de_fin}
-      </span>
-    </div>
-    <div className={eventsItemTaxo}>
-      {evenement.relationships.field_taxonomie_thematique?.length > 0 && (
-        <Link
-          to={evenement.relationships.field_taxonomie_thematique[0].path.alias}
-        >
-          {evenement.relationships.field_taxonomie_thematique[0]?.name}
-        </Link>
-      )}
-    </div>
-    <div className={eventsItemTitle}>
-      <Link to={evenement.path.alias}>{evenement.title}</Link>
-    </div>
-    {evenement.body && (
-      <div
-        className={eventsItemResume}
-        dangerouslySetInnerHTML={{
-          __html: SummaryTransformer(evenement.body, 200),
-        }}
+const Filtres = ({ eventsData, pageContext }) => {
+  const taxoData = eventsData.allTaxonomyTermThematiques.edges
+  const [chooseTaxo, setChooseTaxo] = useState("")
+  return (
+    <>
+      <section className={eventsFilters}>
+        <div className={eventsFilterByTaxo}>
+          <h3>Types d'évènement(s) : </h3>
+          {taxoData.map(({ node }) => {
+            let taxoActiveClass = false
+            if (chooseTaxo === node.name) {
+              taxoActiveClass = true
+            }
+            return (
+              <>
+                <span
+                  className={
+                    taxoActiveClass
+                      ? eventsFiltersTaxoActive
+                      : eventsFiltersTaxo
+                  }
+                  onClick={() => {
+                    if (chooseTaxo === "" || chooseTaxo !== node.name) {
+                      setChooseTaxo(node.name)
+                    } else if (chooseTaxo === node.name) {
+                      setChooseTaxo("")
+                    }
+                  }}
+                >
+                  {node.name}
+                </span>
+              </>
+            )
+          })}
+        </div>
+        <div className={eventsFilterByPeriod}></div>
+        <div className={eventsFilterDelete}></div>
+      </section>
+      <FiltreEvenement
+        eventsData={eventsData}
+        taxoChoisie={chooseTaxo}
+        pageContext={pageContext}
       />
-    )}
-  </>
-)
+    </>
+  )
+}
+
+const FiltreEvenement = ({ eventsData, pageContext, taxoChoisie }) => {
+  const { currentPage, numPages, baseLink } = pageContext
+  let filteredData = []
+  filteredData = eventsData.allNodeEvenements.edges.reduce((acc, node) => {
+    let matchingTaxo = []
+    matchingTaxo = node.node.relationships.field_taxonomie_thematique.filter(
+      taxo => taxo.name === taxoChoisie
+    )
+    if (matchingTaxo.length > 0) {
+      acc.push(node)
+    }
+    return acc
+  }, [])
+  return (
+    <>
+      <section className={listEventsContainer}>
+        <div className={eventsContainer}>
+          <RenderEvenements
+            taxoChoisie={taxoChoisie}
+            eventsData={eventsData}
+            filteredData={filteredData}
+          />
+        </div>
+        <Pagination
+          currentPage={currentPage}
+          numPages={numPages}
+          contextPage={baseLink}
+        />
+      </section>
+    </>
+  )
+}
+
+const EvenementInfos = ({ evenement, taxoChoisie }) => {
+  if (
+    taxoChoisie === "" &&
+    evenement.relationships.field_taxonomie_thematique?.length > 0
+  ) {
+    taxoChoisie = evenement.relationships.field_taxonomie_thematique[0].name
+  }
+  return (
+    <>
+      <div className={eventsItemDate}>
+        <span className={eventsItemDateFrom}>
+          Du {evenement.field_date_de_debut}
+        </span>
+        <span className={eventsItemDateTo}>
+          {" "}
+          au {evenement.field_date_de_fin}
+        </span>
+      </div>
+      <div className={eventsItemTaxo}>
+        <Link to={taxoChoisie}>{taxoChoisie}</Link>
+      </div>
+      <div className={eventsItemTitle}>
+        <Link to={evenement.path.alias}>{evenement.title}</Link>
+      </div>
+      {evenement.body && (
+        <div
+          className={eventsItemResume}
+          dangerouslySetInnerHTML={{
+            __html: SummaryTransformer(evenement.body, 200),
+          }}
+        />
+      )}
+    </>
+  )
+}
 
 const EvenementImg = ({ evenement }) => (
   <div className={eventsItemImg}>
@@ -68,14 +156,15 @@ const EvenementImg = ({ evenement }) => (
   </div>
 )
 
-const RenderEvenements = ({ eventsData }) => {
-  const events = eventsData.allNodeEvenements.edges
+const RenderEvenements = ({ eventsData, filteredData, taxoChoisie }) => {
+  let events =
+    taxoChoisie === "" ? eventsData.allNodeEvenements.edges : filteredData
   return (
     <>
       {events.map(({ node }) => (
         <div className={eventsItem}>
           <EvenementImg evenement={node} />
-          <EvenementInfos evenement={node} />
+          <EvenementInfos taxoChoisie={taxoChoisie} evenement={node} />
         </div>
       ))}
     </>
@@ -83,22 +172,12 @@ const RenderEvenements = ({ eventsData }) => {
 }
 
 const EvenementsTemplate = ({ data, pageContext }) => {
-  const { currentPage, numPages, baseLink } = pageContext
   return (
     <Layout message="Liste des évènements">
       <section className={zoneEventsLibreHaut}>
         <ContenuLibre zoneTaxoLibre="zone_evenements_haut" />
       </section>
-      <section className={listEventsContainer}>
-        <div className={eventsContainer}>
-          <RenderEvenements eventsData={data} />
-        </div>
-        <Pagination
-          currentPage={currentPage}
-          numPages={numPages}
-          contextPage={baseLink}
-        />
-      </section>
+      <Filtres pageContext={pageContext} eventsData={data} />
       <section className={zoneEventsLibreBas}>
         <ContenuLibre zoneTaxoLibre="zone_evenements_bas" />
       </section>
@@ -110,6 +189,13 @@ export default EvenementsTemplate
 
 export const query = graphql`
   query evenementsListQuery($skip: Int!, $limit: Int!) {
+    allTaxonomyTermThematiques {
+      edges {
+        node {
+          name
+        }
+      }
+    }
     allNodeEvenements(sort: { fields: created }, limit: $limit, skip: $skip) {
       edges {
         node {
