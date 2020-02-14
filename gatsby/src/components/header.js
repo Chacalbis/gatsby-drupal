82006b230df5003330cc3ce001261970d64ffeac
Florian TRIBOULIN <ftr@atolcd.com>
2020-02-14 11:53:47 +0100

    Remove 'internal:' in mainMenu links with regex
    Change-Id: Id8f4851d3871f1a753b50f66859593499e89bf31
    Reviewed-on: https://gerrit.atolcd.com/c/ternum-bfc/generateur-sites/+/122184
    Reviewed-by: Valentin Fayard <vfa@atolcd.com>
    Tested-by: Valentin Fayard <vfa@atolcd.com>

--- gatsby/src/components/header.js
+++ gatsby/src/components/header.js
@@ -18,6 +18,14 @@ import {
 import Image from "./image"
 import { useSiteMetadata } from "../hooks/use-site-metadata"
 
+function replaceLink(link) {
+  const regex = new RegExp("internal:")
+  if (regex.test(link)) {
+    return link.replace(regex, "")
+  }
+  return link
+}
+
 const MainMenu = ({ mainMenuData }) => {
   const menuWithoutParent = mainMenuData.filter(
     ({ node }) => !node.drupal_parent_menu_item
@@ -38,7 +46,9 @@ const MainMenu = ({ mainMenuData }) => {
           return (
             <li className={navItem}>
               <span className={navItemtitle}>
-                <Link to={item.node.link.uri}>{item.node.title}</Link>
+                <Link to={replaceLink(item.node.link.uri)}>
+                  {item.node.title}
+                </Link>
               </span>
               {menuLevelTwo.length > 0 && (
                 <div className={subNavContainer}>
@@ -50,20 +60,24 @@ const MainMenu = ({ mainMenuData }) => {
                     )
                     return (
                       <ul className={subNav}>
-                        <Link to={child.node.link.uri}>
+                        <Link to={replaceLink(child.node.link.uri)}>
                           <div className={subNavtitle}>
                             <p>{child.node.title}</p>
                           </div>
                         </Link>
                         {menuLevelThree.length > 0 && (
                           <>
-                            {menuLevelThree.map(lastChild => (
-                              <li className={subNavItemTitle}>
-                                <Link to={lastChild.node.link.uri}>
-                                  {lastChild.node.title}
-                                </Link>
-                              </li>
-                            ))}
+                            {menuLevelThree.map(lastChild => {
+                              return (
+                                <li className={subNavItemTitle}>
+                                  <Link
+                                    to={replaceLink(lastChild.node.link.uri)}
+                                  >
+                                    {lastChild.node.title}
+                                  </Link>
+                                </li>
+                              )
+                            })}
                           </>
                         )}
                       </ul>
